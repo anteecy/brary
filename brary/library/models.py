d73c2c1b4c97d5d31f5723991367f7aace8e57f3
anteecy <47046359+anteecy@users.noreply.github.com>
2019-07-04 16:10:47 -0400

    modified search mechanics

--- brary/library/models.py
+++ brary/library/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-	author_last_name = models.CharField(max_length=25)
-	author_first_name = models.CharField(max_length=25)
+    author_last_name = models.CharField(max_length=25)
+    author_first_name = models.CharField(max_length=25)
 
-	def __str__(self):
-		return self.author_last_name + ", " + author_first_name
+    def __str__(self):
+        return self.author_last_name + ", " + self.author_first_name
 
 class Book(models.Model):
-	author = models.ForeignKey(Author,on_delete=models.CASCADE)
-	book_title = models.CharField(max_length=100)
-	book_descr = models.CharField(max_length=400)
-	book_publish_date = models.DateTimeField('date published')
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    book_title = models.CharField(max_length=100)
+    book_descr = models.CharField(max_length=2000)
+    book_publish_date = models.DateTimeField('date published')
+    book_img = models.CharField(max_length=500, default="")
+
+    def __str__(self):
+        return self.book_title
